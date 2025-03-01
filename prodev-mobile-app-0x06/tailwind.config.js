719a6de5f8cf61ffb07883c7e6676b7771164b82
Asiama Phoebe <phoebeasiama6@gmail.com>
2025-03-01 11:32:49 +0000

    update

--- prodev-mobile-app-0x06/tailwind.config.js
+++ prodev-mobile-app-0x06/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],  theme: {
+  content: ["./App.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./screens/**/*.{js,ts,jsx,tsx}",
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  ],  theme: {
     extend: {},
   },
   plugins: [],
